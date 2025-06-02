8f9fb3d46a3b4412bf099488ee5aa8992fb5a85e
Miroslav Tvrdoň <miroslav.tvrdon@student.tuke.sk>
2025-06-02 13:25:35 +0200

    Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
+import Projects from "./sections/Projects.jsx";
+import Clients from "./sections/Clients.jsx";
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
         <Navbar />
         <Hero />
         <About />
+        <Projects />
+        <Clients />
     </main>
   );
 };
