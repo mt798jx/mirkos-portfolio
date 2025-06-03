02d1594a3ed9e1730f41db655ad54e9abcf5e095
Miroslav Tvrdoň <miroslav.tvrdon@student.tuke.sk>
2025-06-03 09:19:42 +0200

    Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
 import Clients from "./sections/Clients.jsx";
 import Contact from "./sections/Contact.jsx";
+import {Footer} from "./sections/Footer.jsx";
+import Experience from "./sections/Experience.jsx";
 
 const App = () => {
   return (
@@ -13,8 +15,9 @@ const App = () => {
         <About />
         <Projects />
         <Clients />
-
+        <Experience />
         <Contact />
+        <Footer />
     </main>
   );
 };
